e1ac6db9c7382a1dc826ff1bb07d6c873d1417af
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-01-15 19:36:08 -0500

    completed part 1 of mvp task

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-
+import {Route} from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -12,7 +14,9 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+
+      <Route path='/' component={MovieList} />
+      <Route path='/movies' component={Movie} />
     </div>
   );
 };
